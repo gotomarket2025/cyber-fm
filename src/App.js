dfd8cf89d7c581c9b900576f65c2eefa728a6ddd
gotomarket2025 <112641700+gotomarket2025@users.noreply.github.com>
2024-11-03 14:39:52 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PlayList from '../src/components/PlayList/PlayList';
+// import MusicPlayer from '../src/components/MusicPlayer/MusicPlayer';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <PlayList />
+      {/* <MusicPlayer /> */}
+    </div>
+  );
+}
+
+export default App;
